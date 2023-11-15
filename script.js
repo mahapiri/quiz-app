443d6502d10e65e1b504518944f32f7843321b78
Pirathib Mahalingam <pirathib.m@gmail.com>
2023-11-15 22:19:25 +0100

    added functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let question = [
+let questions = [
     {
         "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
@@ -57,19 +57,62 @@ let question = [
     },
 ];
 
+
 let currentQuestions = 0;
 
-function startHtml() {
-    let content = document.getElementById('card');
-    content.innerHTML = '';
-    content.innerHTML = /*html*/`
-        <h5 class="card-title">Welcome to<br>The Awesome HTML Quiz</h5>
-        <p class="card-text"></p>
-        <p class="card-text"><small class="text-body-secondary"></small></p>
-        <button type="button" class="btn btn-primary" onclick="goHtml()">Start Game</button>
-    `
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showCurrentQuestion();
 }
 
-function goHtml() {
- 
+function showCurrentQuestion() {
+
+    if(currentQuestions >= questions.length) {
+        document.getElementById('end-screen').style = '';
+        document.getElementById('question-body').style = 'display: none;'
+    } else {
+        let question = questions[currentQuestions];
+    
+        document.getElementById('question-number').innerHTML = currentQuestions + 1;
+        document.getElementById('questiontext').innerHTML = question.question;
+        document.getElementById('answer_1').innerHTML = question.answer_1;
+        document.getElementById('answer_2').innerHTML = question.answer_2;
+        document.getElementById('answer_3').innerHTML = question.answer_3;
+        document.getElementById('answer_4').innerHTML = question.answer_4;
+    }
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestions];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    let idOfRightAnswer = `answer_${question.right_answer}`;
+
+    if (selectedQuestionNumber == question.right_answer) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestions++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showCurrentQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
